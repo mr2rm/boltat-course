7cbdc06cf46fdc1feceee28dd5b69c3e2e7091ba
Mohammad Reza <mohammadreza.rahmani89@gmail.com>
2019-07-19 11:58:17 +0430

    fetch data and loading added

--- day4-exercise/src/Exercises/UserList/index.js
+++ day4-exercise/src/Exercises/UserList/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import App from './App';
-import UserList from './UserList';
-import families from '../../data.json';
+import React, { useEffect, useState } from "react";
+import App from "./App";
+import UserList from "./UserList";
+import families from "../../data.json";
+import loadingImage from "../../assets/images/loading.svg";
 
 export const Example1 = () => <UserList users={families} />;
-export const Example2 = () => <App />;
+export const Example2 = () => {
+	const [families, setFamilies] = useState([]);
+	useEffect(() => {
+		fetch("http://localhost:3001/persons")
+			.then(resp => resp.json())
+			.then(jsonResp => {
+				setFamilies(jsonResp.data);
+			});
+	}, []);
+
+	// todo: add style to loading
+	return families.length ? (
+		<UserList users={families} />
+	) : (
+		<img src={loadingImage} />
+	);
+};
 export default UserList;
