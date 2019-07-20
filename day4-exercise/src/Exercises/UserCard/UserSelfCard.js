8f66116a583a907f8a58c0991bcfc6ae33ec5bdf
Mohammad Reza <mohammadreza.rahmani89@gmail.com>
2019-07-21 01:29:13 +0430

    exercise 1 solved

--- day4-exercise/src/Exercises/UserCard/UserSelfCard.js
+++ day4-exercise/src/Exercises/UserCard/UserSelfCard.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React from "react";
+import styles from "./UserSelfCard.module.scss";
 
 const UserSelfCard = ({ user }) => {
-  return null;
+	console.log(user);
+	return (
+		<div className={styles.card}>
+			<img src={user.image} className={styles.image} />
+			<div className={styles.imageDescription}>
+				<p>{user.name}</p>
+				<p>{user.surName}</p>
+			</div>
+			<div className={styles.details}>
+				<span>{user.gender}</span>
+				<span className={styles.bullet} />
+				<span>{user.age}</span>
+			</div>
+		</div>
+	);
 };
 export default UserSelfCard;
