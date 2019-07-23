026348d30dba5410323b547f9f3aab3d9a38f0a8
Mohammad Reza <mohammadreza.rahmani89@gmail.com>
2019-07-23 07:38:29 +0430

    exercise 2 solved

--- day4-exercise/src/Exercises/UserCard/UserCard.js
+++ day4-exercise/src/Exercises/UserCard/UserCard.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import families from '../../data.json';
-import UserSelfCard from './UserSelfCard';
+import React from "react";
+import families from "../../data.json";
+import UserSelfCard from "./UserSelfCard";
+import styles from "./UserCard.module.scss";
 
 const UserCard = ({ user }) => {
-  return null;
+	return (
+		<div className={styles.container}>
+			<UserSelfCard user={user} />
+			<div className={styles.card}>
+				<p className={styles.title}>Married to</p>
+				{user.marriedTo.map(id => (
+					<a href={`#${id}`} key={id} className={styles.link}>
+						{id}
+					</a>
+				))}
+			</div>
+			<div className={styles.card}>
+				<p className={styles.title}>Children</p>
+				{user.children.map(id => (
+					<a href={`#${id}`} key={id} className={styles.link}>
+						{id}
+					</a>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default UserCard;
--- day4-exercise/src/Exercises/UserCard/UserSelfCard.js
+++ day4-exercise/src/Exercises/UserCard/UserSelfCard.js
@@ -1,19 +1,21 @@
 import React from "react";
+import classnames from "classnames";
 import styles from "./UserSelfCard.module.scss";
 
 const UserSelfCard = ({ user }) => {
-	console.log(user);
 	return (
-		<div className={styles.card}>
-			<img src={user.image} className={styles.image} />
-			<div className={styles.imageDescription}>
-				<p>{user.name}</p>
-				<p>{user.surName}</p>
-			</div>
-			<div className={styles.details}>
-				<span>{user.gender}</span>
-				<span className={styles.bullet} />
-				<span>{user.age}</span>
+		<div className={styles.container}>
+			<div className={classnames(styles.card, styles.selfCard)}>
+				<img src={user.image} className={styles.image} />
+				<div className={styles.imageDescription}>
+					<p>{user.name}</p>
+					<p>{user.surName}</p>
+				</div>
+				<div className={styles.details}>
+					<span>{user.gender}</span>
+					<span className={styles.bullet} />
+					<span>{user.age}</span>
+				</div>
 			</div>
 		</div>
 	);
